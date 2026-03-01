d90e870c89b1803c3a3c26731c5672ce81021979
juvkha <46568133+juvkha@users.noreply.github.com>
2026-03-01 10:18:00 +0530

    Create Pattern2.js

---
+++ Pattern2.js
@@ -0,0 +1,13 @@
+function met(n) {
+  let m = '';
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j <i+1; j++) {
+      m = m + '*';
+    }
+    console.log(m);
+    m = '';
+  }
+}
+
+met(5);
