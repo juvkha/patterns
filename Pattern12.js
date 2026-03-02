f16e62c14f40f030f6bbdbe73eebfe1a6144ed52
juvkha <46568133+juvkha@users.noreply.github.com>
2026-03-02 10:59:11 +0530

    Create Pattern12.js

---
+++ Pattern12.js
@@ -0,0 +1,17 @@
+function met(n) {
+  let m = '';
+  for (let i = 0; i < n; i++) {
+       for(let j=0; j<i+1; j++){
+           m= m+`${j+1}`;
+       }
+       for(let k=0; k < 2*(n-i+1); k++ ){
+           m=m+" "
+       }
+       for(let l=i+1; l>0; l--){
+           m= m+`${l+0}`;
+       }
+      console.log(m);
+      m="";
+  }
+}
+met(4);
